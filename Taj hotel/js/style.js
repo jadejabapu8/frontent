3cfc7cbc93c3f517de7f39041a6c2342965f9920
jadejabapu8 <jadejamandipsinh300@gmail.com>
2025-11-29 16:27:16 +0530

    commit

--- Taj hotel/js/style.js
+++
@@ -1,6 +0,0 @@
-// let toggle = document.querySelector(".menu-toggle");
-// let nav = document.querySelector(".nav");
-
-// toggle.addEventListener("click", () => {
-//     nav.classList.toggle("show");
-// });
